8af2855bd35c622a0c366242cb6fa1622462c989
yuvaraj2016 <juvaraj2016@gmail.com>
2020-09-29 19:06:25 -0700

    updates

--- routes/web.php
+++ routes/web.php
@@ -19,8 +19,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-    //$request->session()->put('token', $value);
-    //return Http::get('http://restschool.hridham.com/api/getAllAlbums')->json();
+   
 })->name('home');
 Route::get('/dashboard', function () {
     return view('dashboard');
@@ -29,102 +28,26 @@ Route::post('/login', 'UserController@login');
 
 Route::get('/logout', 'UserController@logout')->name('logout')->middleware('checktoken');
 
-Route::resource('albums', 'AlbumController');
-Route::resource('albums.photo', 'PhotoController');
-Route::resource('testimonials', 'TestimonialsController');
+// Route::resource('albums', 'AlbumController');
+// Route::resource('albums.photo', 'PhotoController');
+// Route::resource('testimonials', 'TestimonialsController');
 
 Route::resource('bookings', 'BookingController')->except('index')->middleware('checktoken');
 
 Route::get('booking_list/{page?}','BookingController@index')->name('booking.index')->middleware('checktoken');
 
-Route::get('getprodSubcat/{id}','ProductCategoryController@getsubcategories')->name('getsubcategories')->middleware('checktoken');
-
-Route::get('getallprodSubcat','ProductSubCategoryController@getallsubcategories')->name('getallprodSubcat')->middleware('checktoken');
-
-Route::resource('product_sub_categories', 'ProductSubCategoryController')->except('index')->middleware('checktoken');
-
-Route::get('product_sub_cat_list/{page?}','ProductSubCategoryController@index')->name('product_sub_cat.index')->middleware('checktoken');
-
-
-Route::resource('supplier_categories', 'SupplierCategoryController')->except('index')->middleware('checktoken');
-
-Route::get('supplier_cat_list/{page?}','SupplierCategoryController@index')->name('supplier_cat.index')->middleware('checktoken');
-
-
-Route::resource('vendor_categories', 'VendorCategoryController')->except('index')->middleware('checktoken');
-
-Route::get('vendor_cat_list/{page?}','VendorCategoryController@index')->name('vendor_cat.index')->middleware('checktoken');
 
 Route::resource('status', 'StatusController')->except('index')->middleware('checktoken');
 
 Route::get('status_list/{page?}','StatusController@index')->name('status.index')->middleware('checktoken');
 
-
-Route::resource('ordertype', 'OrdertypeController')->except('index')->middleware('checktoken');
-
-Route::get('ordertype_list/{page?}','OrdertypeController@index')->name('ordertype.index')->middleware('checktoken');
-
-
-
 Route::resource('payment', 'PaymentController')->except('index')->middleware('checktoken');
 
 Route::get('payment_list/{page?}','PaymentController@index')->name('payment.index')->middleware('checktoken');
 
-
-Route::resource('items', 'ItemController')->except('index')->middleware('checktoken');
-
-Route::get('item_list/{page?}','ItemController@index')->name('item.index')->middleware('checktoken');
-
-Route::get('getitemvariants/{id}','ItemController@getitemvariants')->name('getitemvariants')->middleware('checktoken');
-
-Route::get('getallitemvariants','ItemVariantController@getallitemvariants')->name('getallitemvariants')->middleware('checktoken');
-
-
-
-Route::resource('item_variants', 'ItemVariantController')->except('index')->middleware('checktoken');
-
-Route::get('item_variant_list/{page?}','ItemVariantController@index')->name('item_variant.index')->middleware('checktoken');
-
-
-
-Route::resource('item_variants_group', 'ItemVariantgroupController')->except('index')->middleware('checktoken');
-
-Route::get('item_variant_group_list/{page?}','ItemVariantgroupController@index')->name('item_variant_group.index')->middleware('checktoken');
-
-
-
-
-
-
-Route::resource('stock_masters', 'StockMasterController')->except('index')->middleware('checktoken');
-
-Route::get('stock_master_list/{page?}','StockMasterController@index')->name('stock_master.index')->middleware('checktoken');
-
-Route::resource('suppliers', 'SupplierController')->except('index')->middleware('checktoken');
-
-Route::get('supplier_list/{page?}','SupplierController@index')->name('supplier.index')->middleware('checktoken');
-
-
-Route::resource('vendors', 'VendorController')->except('index')->middleware('checktoken');
-
-Route::get('vendor_list/{page?}','VendorController@index')->name('vendor.index')->middleware('checktoken');
-
-Route::get('getvendorStore/{id}','VendorController@getvendorstores')->name('getvendorstores')->middleware('checktoken');
-
-Route::get('getallVendorStore','VendorstoresController@getallvendorstores')->name('getallvendorstores')->middleware('checktoken');
-
-
-Route::resource('vendorstores', 'VendorstoresController')->except('index')->middleware('checktoken');
-
-Route::get('vendorstores_list/{page?}','VendorstoresController@index')->name('vendorstores.index')->middleware('checktoken');
-
-
-
 Route::resource('profile', 'ProfileController')->except('index')->middleware('checktoken');
 
 Route::get('show_profile/{page?}','ProfileController@index')->name('profile.index')->middleware('checktoken');
-
-
 
 Route::put('update_password', 'ProfileController@updatepassword')->name('update_password')->middleware('checktoken');
 
@@ -142,8 +65,6 @@ Route::resource('roles', 'RoleController')->except('index')->middleware('checkto
 Route::get('role_list/{page?}','RoleController@index')->name('role.index')->middleware('checktoken');
 
 
-
-
 Route::resource('permissions', 'PermissionController')->except('index')->middleware('checktoken');
 
 Route::get('permission_list/{page?}','PermissionController@index')->name('permission.index')->middleware('checktoken');
@@ -151,12 +72,6 @@ Route::get('permission_list/{page?}','PermissionController@index')->name('permis
 
 
 Route::resource('settings', 'SettingsController')->except('index')->middleware('checktoken');
-
-
-Route::resource('stock_tracker', 'StockTrackerController')->except('index')->middleware('checktoken');
-
-Route::get('stock_tracker_list/{page?}','StockTrackerController@index')->name('stock_tracker.index')->middleware('checktoken');
-
 
 
 
@@ -168,20 +83,7 @@ Route::resource('assets', 'AssetsController')->except('store')->middleware('chec
 
 Route::get('{slug?}/{id}/edit/assets','AssetsController@editimage')->name('assets.edit')->middleware('checktoken');
 
-// Route::get('product_categories/{page?}', function (Request$page=0) {
-//     echo $page;
-// });
 
-// Route::post('/login', function () {
-// $respose=Http::post('http://restschool.hridham.com/api/login',[
-//     "username"=>"admin",
-//     "password"=>"12345678"
-// ]);
-
-// //Session::put('token',$respose->json()['token']);
-// session()->put('token', $respose->json()['token']);
-//   return dd(session()->get('token'));
-// });
 Route::get('getuser', function () {
     return 1;
 });
