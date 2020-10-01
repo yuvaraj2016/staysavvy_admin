6c3943bf8ccbc18ba72151ff9b2ce73fec6207d9
yuvaraj2016 <juvaraj2016@gmail.com>
2020-09-30 21:12:14 -0700

    hgf

--- app/Http/Controllers/BookingController.php
+++ app/Http/Controllers/BookingController.php
@@ -41,71 +41,11 @@ class BookingController extends Controller
             return redirect()->route('home');
         }
         
-        // try{
-           
-        //     $call = Http::withToken($token)->withHeaders(['Accept'=>'application/vnd.api.v1+json','Content-Type'=>'application/json'])->get(config('global.url') . 'api/prodCat?page='.$page);
-
-        //     $response = json_decode($call->getBody()->getContents(), true);
-            
-        //     // return $response;
-
-        // }catch (\Exception $e){
-        //     //buy a beer
-
-
-        // }
-        // $prodcategories = $response['data'];
-        //  $pagination = $response['meta']['pagination'];
-
-        //   $lastpage = $pagination['total_pages'];
-        //  $current_page = $pagination['current_page'];
-
-        //  if($numOfpages > 1)
-        //  {
-        //     $has_next_page = true;
-        //     $has_previous_page = true;
-        //  }
-        //  if($current_page > 1 && $current_page<= $numOfpages)
-        //  {
-        //     $next_page = $current_page+1;
-
-        //  }
-        //  else if($current_page == 1 && $current_page != $numOfpages){
-        //     $next_page = $current_page+1;
-        //     $has_previous_page = false;
-        //  }
-
-        //  else if($current_page == $numOfpages){
-        //      $next_page = '';
-        //     $has_next_page = false;
-        //  }
-        // //  $next_page = $pagination['total_pages'];
-        //  $next= $pagination['links']['next'];
-
-        //  return  $next_page;
-        // return view(
-        //     'product_category_list', compact(
-        //         'prodcategories', 'pagination','current_page',
-        //         'has_next_page', 'has_previous_page', 'next_page',
-        //     )
-
-            // return view(
-            //     'booking_list', compact(
-            //         'prodcategories', 'pagination','lastpage'
-            //     )
-
+     
              return view('booking_list');
     
 
-        // if (isset($url)) {
-        //     $response = Http::withToken($token)->get($url);
-        // } else {
-        //     $response = Http::withToken($token)->get(config('global.url') . '/api/prodCat');
-        // }
-        // // return $response;
-        // $prodcategories = $response->json();
-
-        // return view('product_category_list', ['prodcategories' => $prodcategories, 'token' => $token]);
+       
     }
 
     /**
